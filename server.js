83394e3be13fc77afca96fff064b56c8e1297368
Hector Osorio <hosorio@pilot.ly>
2016-09-14 11:35:53 -0700

    Add put todos route

--- server.js
+++ server.js
@@ -30,9 +30,7 @@ app.get('/todos/:id', function(req, res) {
 });
 
 app.post('/todos', function(req, res) {
-    var body = req.body;
-
-    body = _.pick(body, 'description', 'completed');
+    var body = _.pick(req.body, 'description', 'completed');
 
     if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(400).send();
@@ -56,6 +54,36 @@ app.delete('/todos/:id', function(req, res) {
     else {
         res.status(404).send({"error": "No todo found"});
     }
+});
+
+app.put('/todos/:id', function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (!matchedTodo) {
+        res.status(404).send({"error": "No todo found"});
+    }
+
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    }
+    else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    }
+    else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    
+    res.json(matchedTodo);
 });
 
 app.listen(PORT, function() {
